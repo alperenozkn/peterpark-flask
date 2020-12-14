272e6047fd06a357e7f3263412a5d4051c85a166
alperenozkn <m.alperenozkn@gmail.com>
2020-12-14 15:28:51 +0100

    Change project structure and add api blueprint

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from app import create_app
 
-
-app = Flask(__name__)
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app
---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp)
+
+    return app
