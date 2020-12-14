1d0a5ee5884d9bf4f7a0b72381c528de93c81eee
alperenozkn <m.alperenozkn@gmail.com>
2020-12-14 17:09:34 +0100

    Add shell context

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from app import create_app
+from app import create_app, db
+from app.models import Plate
 
 app = create_app()
 
-if __name__ == '__main__':
-    app
+@app.shell_context_processor
+def make_shell_context():
+    return {'app': app, 'db': db, 'Plate': Plate}
+
